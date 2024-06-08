209df9a91af72e1a88e079dfa35bebf6cf1e65d9
Bryan De La Cruz <bryan.delacruza@gmail.com>
2024-06-07 21:57:39 -0500

    S3-ComboBox

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -17,6 +17,7 @@ const links = [
   { name: "carousel", href: "carousel" },
   { name: "checkbox", href: "checkbox" },
   { name: "command", href: "command" },
+  { name: "comboBox", href: "comboBox" },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
